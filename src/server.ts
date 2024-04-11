5b6c3f22214da07e77954b966ae5270d3dcd0d96
campos-maravilla <antonycampos25@outlook.com>
2024-04-11 08:19:48 -0600

    routing-en-express,get,post.put y delete

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,19 @@ const server=express()
 
 //routing
 server.get('/',(req,res)=>{
-    res.send('Hola Mundo en Express')
+    res.json('Desde GET')
+})
+
+server.post('/',(req,res)=>{
+    res.json('Desde POST')
+})
+
+server.put('/',(req,res)=>{
+    res.json('Desde PUT')
+})
+
+server.delete('/',(req,res)=>{
+    res.json('Desde DELETE')
 })
 
 export default server
