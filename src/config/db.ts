62dc8db09f3427767a52cfe4b12e9856e6a4e8e6
campos-maravilla <antonycampos25@outlook.com>
2024-04-11 15:10:25 -0600

    creando-modelos-en-la-base-de-datos ,se crea la carpeta models en src y se crea el archivo Product.model.ts,ademas se agreaga experimentalDecorators,emitDecoratorMetadata como true en tsconfig.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,11 @@
 
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
 dotenv.config()
 
 //se crea la conexion de la base de datos con dbeaver
-const db=new Sequelize(process.env.DATABASE_URL)
+const db=new Sequelize(process.env.DATABASE_URL!,{
+    models:[__dirname+'/../models/**/*.ts']
+})
 
 export default db 
