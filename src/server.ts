14c2a49abd33c6777986bcc037878486b5d3164d
campos-maravilla <antonycampos25@outlook.com>
2024-04-11 08:33:57 -0600

    separando-las-rutas-en-su-propio-archivo

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,8 @@
 import express from 'express'
+import router from './router'
 
 const server=express()
 
-//routing
-server.get('/',(req,res)=>{
-    res.json('Desde GET')
-})
-
-server.post('/',(req,res)=>{
-    res.json('Desde POST')
-})
-
-server.put('/',(req,res)=>{
-    res.json('Desde PUT')
-})
-
-server.delete('/',(req,res)=>{
-    res.json('Desde DELETE')
-})
+server.use('/api/products',router)
 
 export default server
