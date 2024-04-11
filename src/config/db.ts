d1db3c5a38077f57ebd02e81b017df8c50786fa5
campos-maravilla <antonycampos25@outlook.com>
2024-04-11 10:43:38 -0600

    conectando-la-bd-con-dbeaver ,se descarga dbeaver,se configura el host,database,nombre de usuario y contraseña

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'
 dotenv.config()
 
-
+//se crea la conexion de la base de datos con dbeaver
 const db=new Sequelize(process.env.DATABASE_URL)
 
 export default db 
