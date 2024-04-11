0c4d1287223becb716d6b7e7201ee3c3dd7a5336
campos-maravilla <antonycampos25@outlook.com>
2024-04-11 15:24:14 -0600

    creando-una-funcion-para-crear-productos,se crea la carpeta de handlers en src y se crea el archivo de product.ts y se crea la funcion de createPorduct y se llama en router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,14 @@
 import { Router } from "express"
+import { createProduct } from "./handlers/product"
+createProduct
 const router=Router()
+
 //routing
 router.get('/',(req,res)=>{
     res.json('Desde GET')
 })
 
-router.post('/',(req,res)=>{
-    res.json('Desde POST')
-})
+router.post('/',createProduct)
 
 router.put('/',(req,res)=>{
     res.json('Desde PUT')
