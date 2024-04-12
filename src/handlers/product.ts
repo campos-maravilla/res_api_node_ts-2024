47a2c75cc1a104de87e0f22d7b9d4490fd6945ff
campos-maravilla <antonycampos25@outlook.com>
2024-04-12 10:37:33 -0600

    que-es-middleware-en-express,se  crea la carpeta de middleware y se crea el archivo de index.ts ahi se pasa lo de que es errors

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,19 +1,15 @@
 import { Request,Response } from "express"
 import Product from "../models/Product.model"
-import {validationResult } from "express-validator"
 
 
 export const createProduct=async(req:Request,res:Response)=>{
-   //const product=new Product(req.body)
-   
-   
-   let errors=validationResult(req)
-   if(!errors.isEmpty()){
-    return res.status(400).json({errors:errors.array()})
-   }
-   
-   const product=await Product.create(req.body)
+   //const product=new Product(req.body) 
+ try {
+  const product=await Product.create(req.body)
   //const saveProduct=await  product.save()
    
     res.json({data:product})
+ } catch (error) {
+  console.log(error)
+ }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { createProduct } from "./handlers/product"
 import { body } from "express-validator"
+import { handleInputErrors } from "./middleware"
 
 const router=Router()
 
@@ -14,10 +15,11 @@ router.post('/',
  body('name')
  .notEmpty().withMessage('El nombre del producto no puede ir vacio'),
  
- body('precio')
+ body('price')
  .isNumeric().withMessage('Valor no válido')
  .notEmpty().withMessage('El precio del producto no puede ir vacio')
  .custom(value=>value>0).withMessage('Precio no válido'),
+ handleInputErrors,
 createProduct
 )
 
