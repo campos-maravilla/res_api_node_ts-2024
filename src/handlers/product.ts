24ae82c572fc541d495fda45ce8a07875cd88d67
campos-maravilla <antonycampos25@outlook.com>
2024-04-12 11:43:51 -0600

    creando-el-endpoint-para-obtener-todos-los-productos

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,6 +2,21 @@ import { Request,Response } from "express"
 import Product from "../models/Product.model"
 
 
+export const getProducts=async(req:Request,res:Response)=>{
+  
+  try {
+    const products=await Product.findAll({
+    order:  [
+        ['price','DESC']
+      ]
+   // attributes:{exclude:['updatedAt','createdAt','availability']}
+    })
+    res.json({data:products})
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const createProduct=async(req:Request,res:Response)=>{
    //const product=new Product(req.body) 
  try {
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,12 @@
 import { Router } from "express"
-import { createProduct } from "./handlers/product"
+import { createProduct, getProducts } from "./handlers/product"
 import { body } from "express-validator"
 import { handleInputErrors } from "./middleware"
 
 const router=Router()
 
 //routing
-router.get('/',(req,res)=>{
-    res.json('Desde GET')
-})
+router.get('/',getProducts)
 
 router.post('/',
  // validación
