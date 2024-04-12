e7918bd151e860c5650ccb26d56c43c74fc8c80a
campos-maravilla <antonycampos25@outlook.com>
2024-04-11 19:05:06 -0600

    validando-crear-producto-en-el-servidor,se instala express-validator y se llama en product.ts

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,9 +1,25 @@
 import { Request,Response } from "express"
 import Product from "../models/Product.model"
+import { check,validationResult } from "express-validator"
 
 
 export const createProduct=async(req:Request,res:Response)=>{
    //const product=new Product(req.body)
+   // validación
+   await check('name')
+   .notEmpty().withMessage('El nombre del producto no puede ir vacio')
+   .run(req)
+   await check('precio')
+   .isNumeric().withMessage('Valor no válido')
+   .notEmpty().withMessage('El precio del producto no puede ir vacio')
+   .custom(value=>value>0).withMessage('Precio no válido')
+   .run(req)
+   
+   let errors=validationResult(req)
+   if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()})
+   }
+   
    const product=await Product.create(req.body)
   //const saveProduct=await  product.save()
    
