8c6b0b4c37657824a52637d4d127260c0eaaa9f6
campos-maravilla <antonycampos25@outlook.com>
2024-04-14 07:53:27 -0600

    probando-validacion-de-crear-producto

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -3,12 +3,21 @@ import request  from "supertest";
 import server from "../../server";
 
 describe('POST /api/products',()=>{
-})
-it('should create a new product',async()=>{
-    const response=await request(server).post('/api/products').send({
+    it('should display validation errors',async()=>{
+        const response=await request(server).post('/api/products').send({})
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+        
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+    
+    it('should create a new product',async()=>{
+        const response=await request(server).post('/api/products').send({
             name:"Teclado - Testing",
             price:100       
-          
+            
     })
     expect(response.status).toEqual(201)
     expect(response.body).toHaveProperty('data')
@@ -16,5 +25,6 @@ it('should create a new product',async()=>{
     expect(response.status).not.toBe(404)
     expect(response.status).not.toBe(200)
     
-    expect(response.body).not.toHaveProperty('error')
+    expect(response.body).not.toHaveProperty('errors')
+})
 })
