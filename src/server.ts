e890223bfa873ec3905e6a1bb7ec7db8eb439a01
campos-maravilla <antonycampos25@outlook.com>
2024-04-15 14:24:16 -0600

    forzar-errores-para-probar-tus-catchs,sen server.test.ts se llama a connectDB y a db desde ../config/db,luego se hace la prueba,pero antes se llama a mock de esta manera jest.mock('../config/db')

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import router from './router'
 import db from './config/db'
 import colors from 'colors'
 //Conectar a base de datos 
-async function connectDB(){
+export async function connectDB(){
     try {
         await db.authenticate()
         db.sync()
