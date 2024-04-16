77745ed284cfea7d749f503ccea83cb71c233af4
campos-maravilla <antonycampos25@outlook.com>
2024-04-16 08:01:55 -0600

    documentando-todos-los-productos

--- src/router.ts
+++ src/router.ts
@@ -30,7 +30,27 @@ const router=Router()
  *                       example: true
  */
 
-//routing
+
+/**
+ * @swagger
+ * /api/products:
+ *      get:
+ *          summary: Get list of products
+ *          tags:
+ *              - Products
+ *          description: Return a list of products 
+ *          responses:
+ *              200:
+ *                  description: Successful response
+ *                  content:
+ *                      application/json:
+ *                           schema:
+ *                                type: array
+ *                                items:    
+ *                                    $ref: '#/components/schemas/Product'     
+ * 
+ * 
+ */
 router.get('/',getProducts)
 router.get('/:id',
 param('id').isInt().withMessage('ID no vàlido'),
