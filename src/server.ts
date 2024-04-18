56507e2ea65721129f218f3257caff932a381b5a
campos-maravilla <antonycampos25@outlook.com>
2024-04-18 07:58:24 -0600

    configurando-cors-para-permitir-conexion-del-frontend

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import db from './config/db'
 import colors from 'colors'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec,{swaggerUiOptions} from './config/swagger'
+import cors,{CorsOptions} from 'cors'
+
 //Conectar a base de datos 
 export async function connectDB(){
     try {
@@ -21,6 +23,20 @@ connectDB()
 // Instancia de express 
 const server=express()
 
+// Permitir conexiones 
+const corsOptions:CorsOptions={
+    origin:function(origin,callback){
+        //console.log(origin)
+        if(origin===process.env.FRONTEND_URL){
+          //  console.log('Permitir...')
+          callback(null,true)
+        }else{
+           // console.log('Denegar...')
+           callback(new Error('Error de CORS'))
+        }
+    }
+}
+server.use(cors(corsOptions))
 // leer datos de formularios
 server.use(express.json())
 
