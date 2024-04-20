664b5594b5a58b4f8a12feb4b95fdaa68d454c24
campos-maravilla <antonycampos25@outlook.com>
2024-04-19 20:03:03 -0600

    configurando-package-json-tsconfig-json-para-subir-el-backend

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sumar = void 0;
-function sumar() {
-    console.log(3 + 2);
-}
-exports.sumar = sumar;
-//# sourceMappingURL=server.js.map
--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 //se crea la conexion de la base de datos con dbeaver
 const db=new Sequelize(process.env.DATABASE_URL!,{
-    models:[__dirname+'/../models/**/*.ts'],
+    models:[__dirname+'/../models/**/*'],
     logging:false 
 })
 
